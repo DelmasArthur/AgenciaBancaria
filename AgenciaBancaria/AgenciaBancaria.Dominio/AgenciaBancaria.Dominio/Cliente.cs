f80f98c39f84071c075a4791ea2f00ffb79c01c0
DelmasArthur <arthurvdelmas@gmail.com>
2021-10-03 18:06:03 -0300

    Variaveis referentes aos dados do cliente

---
+++ AgenciaBancaria/AgenciaBancaria.Dominio/AgenciaBancaria.Dominio/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class Cliente
+    {
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string RG { get; private set; }
+        //Endereço
+        public string Logradouro { get; set; }
+        public string CEP { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+    }
+}
